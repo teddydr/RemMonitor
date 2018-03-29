39688eaf412c7e3d062032066d47b4506fb0370c
Teddy Rivera <teddydr@yahoo.com>
2018-03-29 23:38:44 +0800

    Add project files.

---
+++ RemMonitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
