94941e8b2e4c15d47801c5c4878bbb528ed0c5ef
Teddy Rivera <teddydr@yahoo.com>
2018-03-30 00:05:38 +0800

    ...

---
+++ RemMonitor/LogContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace RemMonitor
+{
+    class LogContext : DbContext
+    {
+        public DbSet<LogDB> LogsDB { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=RemittanceRefresh.monitor;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ RemMonitor/LogDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RemMonitor
+{
+    public class LogDB
+    {
+        [Key]
+        public int RecID { get; set; }
+
+        public string TopicName { get; set; }
+        public DateTime DateStamp { get; set; }
+
+
+    }
+}
